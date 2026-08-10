a7f0bf99f0fc3877487eced87aaa58c4a0a9d8a7
tianqy121 <tianqy121@gmail.com>
2026-08-10 18:08:04 +0800

    Delete app directory

--- app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-import { requireChatGPTUser } from "../chatgpt-auth";
-import AdminClient from "./admin-client";
-
-export default async function AdminPage() {
-  const user = await requireChatGPTUser("/admin");
-  return <AdminClient displayName={user.displayName} />;
-}
